7298cd474ef19f9b4a2ef838979949aaa367b3dd
Jeevan <trainee.jeevan.george@sigmalabs.co.uk>
2024-11-27 17:34:23 +0000

    Created terraform code for an execution role for our task def

--- terraform-etl-iterator/main.tf
+++ terraform-etl-iterator/main.tf
@@ -3,9 +3,36 @@ provider "aws" {
 }
 
 
+resource "aws_iam_role" "ecs_role" {
+  name               = "connect4-ETL-task-exec-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs.amazonaws.com"
+        }
+        Effect    = "Allow"
+      }
+    ]
+  })
+}
 
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_readonly" {
+  role       = aws_iam_role.ecs_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
 
+resource "aws_iam_role_policy_attachment" "ecs_full_access" {
+  role       = aws_iam_role.ecs_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+}
 
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs_full_access" {
+  role       = aws_iam_role.ecs_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
 
 
 
