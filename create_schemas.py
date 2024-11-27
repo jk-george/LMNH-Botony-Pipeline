b04399faea11bd303cbc395ed6e749b4f0937f6d
gem09lo <gemmaw09lo@gmail.com>
2024-11-27 12:21:12 +0000

    added file that runs schema.sql and creates schemas in the RDS solves #27

---
+++ create_schemas.py
@@ -0,0 +1,39 @@
+"""Executes schema SQL script to set up the database schema"""
+import logging
+from connect_to_database import get_connection, get_cursor
+
+
+def configure_logging() -> None:
+    """Configure logging"""
+    logging.basicConfig(
+        filename='db_schema_creation.log',
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+
+
+def run_schema_script() -> None:
+    """Runs the schema.sql script to create initial tables"""
+    conn = get_connection()
+
+    if conn:
+        cursor = get_cursor(conn)
+
+        try:
+            with open('schema.sql', 'r') as file:
+                sql_script = file.read()
+
+            cursor.execute(sql_script)
+            conn.commit()
+            logging.info("Schema executed successfully.")
+        except Exception as e:
+            logging.error(f"Error executing schema.sql: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+            logging.info("Database connection closed.")
+
+
+if __name__ == "__main__":
+    configure_logging()
+    run_schema_script()
