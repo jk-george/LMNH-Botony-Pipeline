54705d4b675b51e705c58873f71c08068c2e3bbd
emily-curtis <emily.curtis99@yahoo.co.uk>
2024-11-28 15:29:23 +0000

    added emails

--- email_sender.py
+++ email_sender.py
@@ -59,7 +59,7 @@ def send_email_alert(ses: BaseClient, sender_email: str, plant_data: dict[str, s
     try:
         ses.send_email(
             Source=sender_email,
-            Destination={'ToAddresses': [plant_data['botanist_email']]},
+            Destination={'ToAddresses': ['trainee.emily.curtis@sigmalabs.co.uk']},
             Message={
                 'Subject': {'Data': subject},
                 'Body': {'Text': {'Data': body}}
@@ -90,7 +90,7 @@ def check_and_alert_unhealthy_plants(plant_data_list: list[dict], ses: BaseClien
         except ValueError as e:
             logging.error(f"Invalid data format in plant entry: {e}")
 
-def main() -> None:
+def main_email_alerts() -> None:
     """Main function to monitor plant health and send alerts."""
     try:
         logging.basicConfig(
@@ -99,7 +99,7 @@ def main() -> None:
             filename='email_sender.log',
             filemode='a'
         )
-        load_dotenv()
+       
 
         config = get_config()
         ses = get_ses_client(config['aws_region'])
@@ -110,5 +110,6 @@ def main() -> None:
         logging.error(f"Error occurred in main: {e}")
 
 if __name__ == '__main__':
-    main()
+    load_dotenv()
+    main_email_alerts()
 
