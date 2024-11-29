a1b75624fdb4ec725170fd96e21953cb304d80d3
Jeevan <trainee.jeevan.george@sigmalabs.co.uk>
2024-11-29 09:42:55 +0000

    Created a new ECR on terraform for the image that will hold the long term storage transfer script

--- terraform-ecr-and-s3/main.tf
+++ terraform-ecr-and-s3/main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-
-resource "aws_ecr_repository" "my_ecr_repo" {
-  name                 = var.ECR_NAME  
+# ECR to hold the image that runs the short -> long term data transfer
+resource "aws_ecr_repository" "ecr_for_long_term_storage" {
+  name                 = var.TRANSFER_DATA_ECR_NAME
   image_tag_mutability = "MUTABLE"  
   image_scanning_configuration {
     scan_on_push = true  
@@ -16,6 +16,23 @@ resource "aws_ecr_repository" "my_ecr_repo" {
 }
 
 
+
+# ECR to hold the image that runs the etl process
+resource "aws_ecr_repository" "ecr_for_etl" {
+  name                 = var.ETL_ECR_NAME  
+  image_tag_mutability = "MUTABLE"  
+  image_scanning_configuration {
+    scan_on_push = true  
+  }
+
+  lifecycle {
+    prevent_destroy = false  
+  }
+}
+
+
+
+# S3: Long term storage solution
 resource "aws_s3_bucket" "long_term_storage_bucket" {
   bucket = var.BUCKET
   force_destroy = true
--- terraform-ecr-and-s3/variables.tf
+++ terraform-ecr-and-s3/variables.tf
@@ -1,4 +1,4 @@
-variable ECR_NAME {
+variable TRANSFER_DATA_ECR_NAME {
     type = string
     description = "Holds the ECR Name."
 }
@@ -6,4 +6,9 @@ variable ECR_NAME {
 variable BUCKET {
     type = string
     description = "Holds the Bucket name for files to be sent to."
+}
+
+variable ETL_ECR_NAME {
+    type = string
+    description = ""
 }
